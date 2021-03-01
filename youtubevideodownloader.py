fd12c3cef287498dc969447ae293ce830b11ff32
Ryan <72277073+Ryan-de@users.noreply.github.com>
2021-03-01 17:33:07 +0200

    Add files via upload

---
+++ youtubevideodownloader.py
@@ -0,0 +1,26 @@
+#Import Liabraries
+from tkinter import *
+from pytube import YouTube
+
+#Display Window
+root =Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("Youtube Video Download Manager")
+Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
+
+#Field To Enter Link
+link = StringVar()
+Label(root,text = 'Paste link here:', font= 'arial 15 bold').place(x=160 , y=60)
+link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
+
+#Function To Start Downloading
+def Downloader():     
+    url =YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180 , y = 210)  
+
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+
+root.mainloop()
